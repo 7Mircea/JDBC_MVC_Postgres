b51dedb5b60275070ce415e9f3e392683354421c
Gheoace Mircea <7mircea.c@gmail.com>
2022-10-17 10:13:59 +0300

    add /ces_by_type endpoint

--- src/main/java/com/example/jpa_hikari_jdbc/repository/CustomerEmployeeSupplierRepository.java
+++ src/main/java/com/example/jpa_hikari_jdbc/repository/CustomerEmployeeSupplierRepository.java
@@ -3,5 +3,8 @@ package com.example.jpa_hikari_jdbc.repository;
 import com.example.jpa_hikari_jdbc.model.CustomerEmployeeSupplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface CustomerEmployeeSupplierRepository extends JpaRepository<CustomerEmployeeSupplier,Integer> {
+    Set<CustomerEmployeeSupplier> findCustomerEmployeeSupplierByTypeCES(char c);
 }
